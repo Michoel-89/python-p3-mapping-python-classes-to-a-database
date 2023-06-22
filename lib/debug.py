a0fee907ffeffa965890c0b00c236e7c1bfeff55
michoel <michoelr111@gmail.com>
2023-06-22 11:35:11 -0400

    complete

--- lib/debug.py
+++ lib/debug.py
@@ -5,4 +5,13 @@ from song import Song
 
 
 if __name__ == '__main__':
+    hello = Song("Hello", "25")
+    hello.save()
+
+    despacito = Song("Despacito", "Vida")
+    despacito.save()
+
+    hello.id
+    
+    despacito.id
     import ipdb; ipdb.set_trace()
